c483f81c1b90694dbf3df7a54858452f29bb485e
abubakarpro <linktobakar@gmail.com>
2025-03-18 13:25:46 +0500

    implement functionality to upload multiple images endpoint

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from './cloudinary.service';
 
 @Controller('cloudinary')
 export class CloudinaryController {
   constructor(private readonly cloudinaryService: CloudinaryService) {}
 
-  @Post('upload')
+  @Post('upload/single')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+  async uploadSingleImage(@UploadedFile() file: Express.Multer.File) {
     const result = await this.cloudinaryService.uploadImage(file);
     return { url: result.url };
+  }
+
+  @Post('upload/multiple')
+  @UseInterceptors(FilesInterceptor('files', 10))
+  async uploadMultipleImages(@UploadedFiles() files: Express.Multer.File[]) {
+    const results = await this.cloudinaryService.uploadMultipleImages(files);
+    return { urls: results.map(result => result.url) };
   }
 }
