908551f6ccfdf8d80afdc7a3db80efbb1cf6880c
abubakarpro <linktobakar@gmail.com>
2025-04-09 13:55:19 +0500

    update prisma services

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,7 +1,15 @@
-
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient { }
-      
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect(); // connect when module initializes
+    console.log('Prisma connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect(); // cleanly disconnect on shutdown
+    console.log('Prisma disconnected');
+  }
+}
